cd553d2667bc892f1ece58b79c023c7751a50c22
Alexei Online <alexei.online.work@gmail.com>
2017-03-07 23:52:35 +0300

    add 8 marta

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.get('/8marcha', function (req, res) {
+	res.sendFile(__dirname + '/public/8marcha.html');
+});
+
 app.listen(3001, function () {
   console.log('App listening on port 3001!');
 });
